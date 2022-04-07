cd3177f3fdbab3963feef0ffa4e5ea9e3346c7b1
Zack Barbari <zackb@unc.edu>
2022-04-07 19:42:07 -0400

    Implemented updating case/death numbers for COVID

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -12,3 +12,27 @@ function showText(current) {
 
     }
 }
+
+const covid_api_url = "https://disease.sh/v3/covid-19/all"
+
+fetch(covid_api_url)
+.then((response) => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error("COVID API RESPONSE ERROR");
+    }
+})
+.then(data => {
+    console.log(data);
+    displayCOVID(data)
+})
+.catch((error) => console.error("FETTCH ERROR: ", error));
+
+//display function
+function displayCOVID(data) {
+    document.getElementById("newCases").innerHTML = data.todayCases
+    document.getElementById("newDeaths").innerHTML = data.todayDeaths
+    document.getElementById("cases").innerHTML = data.cases
+    document.getElementById("deaths").innerHTML = data.deaths
+}
